26b03e3a1ae2022b09eb51a7e544169bca54ebe9
Sven Casimir <sven@fewking.de>
2022-05-03 16:41:22 +0200

    add debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ echo Setting up kotlin bootstrap
 cat /data/task-definition.json.tpl
 cat /data/supervisord/service.ini
 
-envsubst /data/task-definition.json.tpl > $GITHUB_WORKSPACE/task-definition.json
-envsubst /data/supervisord/service.ini > $GITHUB_WORKSPACE/etc/supervisor.d/$SERVICE.ini
+envsubst < /data/task-definition.json.tpl > $GITHUB_WORKSPACE/task-definition.json
+envsubst < /data/supervisord/service.ini > $GITHUB_WORKSPACE/etc/supervisor.d/$SERVICE.ini
 
 cat $GITHUB_WORKSPACE/task-definition.json
 cat $GITHUB_WORKSPACE/etc/supervisor.d/$SERVICE.ini
